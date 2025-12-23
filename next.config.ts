cb010f751dece832d8ba8f296d60f8d1e9ae0487
20127045- Võ Đình Khôi <96615959+vdkhoi20@users.noreply.github.com>
2025-12-23 13:51:01 +0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/CPAM.github.io",
+  basePath: "/CPAM.github.io/",
   assetPrefix: "/CPAM.github.io/",
   images: {
     unoptimized: true,
